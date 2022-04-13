eac24ab58285a65034d1c05bef435875af0bc39a
velarina <velarina2014@gmail.com>
2022-04-13 10:31:23 +0700

    fix

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/css/sb-admin-2.css'
 import 'bootstrap/dist/css/dashboard.css'
 import 'bootstrap/dist/css/dashboard.rtl.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
--- src/view/auth/Login.vue
+++ src/view/auth/Login.vue
@@ -54,5 +54,68 @@ export default {
 </template>
 
 <style>
-
+.box-center-v {
+  margin-top: 25%;
+  transform: translateY(-25%);
+}
+.bg-danau {
+  background: url("https://d34ip4tojxno3w.cloudfront.net/app/uploads/Rowing-a-boat-in-a-blue-lake-in-Finland.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+}
+.bg-flat {
+  background-color: #e5e5e5;
+}
+.efek-kaca {
+  background-color: rgba(0, 0, 0, 0.4);
+  backdrop-filter: blur(20px);
+}
+form.user .form-group {
+  position: relative;
+  margin-bottom: 25px;
+}
+form.user .form-group label {
+  position: absolute;
+  top: -6px;
+  left: 14px;
+  font-size: 10px;
+  padding: 0 5px;
+  background-color: #FFF;
+}
+form.user.login .form-group {
+  margin: 40px 0;
+}
+form.user.login .form-group label {
+  background-color: transparent;
+  top: -25px;
+  font-size: 14px;
+}
+.cek-detail {
+  background-color: #222;
+}
+.form-group small {
+  font-size: 11px;
+}
+.form-group p {
+  font-weight: bold;
+  font-size: 18px;
+}
+.form-control {
+  display: block;
+  width: 100%;
+  height: calc(1.5em + 0.75rem + 2px);
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  background-clip: padding-box;
+  border-radius: 0.35rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+form.user .form-control-user {
+  font-size: 0.8rem;
+  border-radius: 10rem;
+  padding: 1.5rem 1rem;
+}
 </style>
