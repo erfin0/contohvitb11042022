d843b868dda88e4843a27c7b896ac44595b1cb8f
velarina <velarina2014@gmail.com>
2022-04-13 09:52:34 +0700

    admin page (dashboard, product, team memmber)

--- src/view/page/admin/Products.vue
+++ src/view/page/admin/Products.vue
@@ -1,59 +1,48 @@
 <template>
 <h2>Products</h2>
+<div class="d-grid gap-2 d-md-block">
+<button type="button" class="btn btn-primary btn-sm">Add Products</button>
+<button type="button" class="btn btn-light btn-sm">Delete</button>
+</div>
       <div class="table-responsive">
         <table class="table">
           <thead class="table-dark">
             <tr>
-              <th scope="col">ID</th>
               <th scope="col">Name</th>
-              <th scope="col">Total</th>
               <th scope="col">Price</th>
-              <th scope="col">Header</th>
+              <th scope="col">COGS</th>
+              <th scope="col">Inventory</th>
+              <th scope="col">Order</th>
+              <th scope="col">Paid</th>
+              <th scope="col">Paid Ratio</th> 
+              <th scope="col">Qty Sold</th>
+              <th scope="col">Net Revenue</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>0100</td>
               <td>light saber</td>
-              <td>200</td>
               <td>375.000</td>
-              <td>text</td>
+              <td>200</td>
+              <td>30</td>
+              <td>4</td>
+              <td>2</td>
+              <td>70%</td>
+              <td>10</td>
+              <td>1000</td>
             </tr>
             <tr>
-              <td>1,002</td>
-              <td>placeholder</td>
-              <td>irrelevant</td>
-              <td>visual</td>
-              <td>layout</td>
+              <td>light saber</td>
+              <td>375.000</td>
+              <td>200</td>
+              <td>30</td>
+              <td>4</td>
+              <td>2</td>
+              <td>70%</td>
+              <td>10</td>
+              <td>1000</td>
             </tr>
-            <tr>
-              <td>1,003</td>
-              <td>data</td>
-              <td>rich</td>
-              <td>dashboard</td>
-              <td>tabular</td>
-            </tr>
-            <tr>
-              <td>1,003</td>
-              <td>information</td>
-              <td>placeholder</td>
-              <td>illustrative</td>
-              <td>data</td>
-            </tr>
-            <tr>
-              <td>1,004</td>
-              <td>text</td>
-              <td>random</td>
-              <td>layout</td>
-              <td>dashboard</td>
-            </tr>
-            <tr>
-              <td>1,005</td>
-              <td>dashboard</td>
-              <td>irrelevant</td>
-              <td>text</td>
-              <td>placeholder</td>
-            </tr>
+
           </tbody>
         </table>
       </div>
@@ -69,6 +58,11 @@
 h2{
   max-width: 1200px;
   margin: auto;
+}
+button{
+  margin-left: 10px;
+  margin-top: 25px;
+  margin-bottom: 10px;
 }
 table{
   max-width: 1200px;
