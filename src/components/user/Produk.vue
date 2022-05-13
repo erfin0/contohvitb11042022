34ed726f395ab5fa185e24a3a53ee536df417837
Osixty <alzidan057@gmail.com>
2022-05-13 15:44:22 +0700

    produk, cart

--- src/components/user/Produk.vue
+++ src/components/user/Produk.vue
@@ -12,12 +12,12 @@ const countt = ref(0);
 
 const router = useRouter();
 onMounted(() => {
- // a(limit.value, offset.value);
- // console.log('The id is: ' + router.params.slug);
+  // a(limit.value, offset.value);
+  // console.log('The id is: ' + router.params.slug);
 });
 
 function a(alimit, aoffset) {
-  axios 
+  axios
     .get(`http://api.everest-app.com/api/${router.params.slug}`)
     .then((result) => {
       produks.value = result.data;
@@ -47,7 +47,81 @@ function abbreviateNumber(value) {
 </script>
 
 <template>
-tes 
+  <main class="tp-1">
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <div class="row">
+
+              <!-- image produck -->
+              <div class=" col-lg-4 col-md-4 col-sm-12">
+                <img class="card-img " src="https://via.placeholder.com/640x480.png/00bb99?text=animals+occaecati"
+                  alt="Eum magnam nihil sint.">
+              </div>
+              <!-- product detail -->
+              <div class="ms-5 col-lg-5 col-md-5 col-sm-12 ">
+                <form action="" class="ps-lg-4">
+                  <h2 class="mt-4 mb-1">Nama product</h2>
+                  <div class="mt-3">
+                    <i class="fa fa-star text-warning"></i>
+                    <i class="fa fa-star text-warning"></i>
+                    <i class="fa fa-star text-warning"></i>
+                    <i class="fa fa-star text-warning"></i>
+                    <i class="fa-regular fa-star text-warning"></i>
+                  </div>
+                  <div class="mt-4">
+                    <h5>
+                      <span>Ready</span>
+                    </h5>
+                  </div>
+                  <!-- price -->
+                  <div class="mt-4">
+                    <h6 class="font-14">Harga :</h6>
+                    <h3>Rp.10.000</h3>
+                  </div>
+                  <!--  -->
+                  <div class="mt-4">
+                    <h6 class="mt-2">Jumlah</h6>
+                    <div class="d-flex">
+                      <input type="number" min="1" class="form-control" placeholder="Qty" value="1" style="width: 90px;">
+                      <button type="button" class="btn btn-danger ms-2"><i class="fa fa-cart-shopping"></i> Add to cart</button>
+                    </div>
+                  </div>
+                  <div class="mt-4">
+                    <h6>Deskripsi:</h6>
+                    <p>
+                      It is a long established fact that a reader will be distracted by the readable content of a page when
+                      looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of
+                      letters, as opposed to using 'Content here, content here', making it look like readable English.
+                    </p>
+                  </div>
+                  <div class="mt-5">
+                    <div class="row">
+                      <div class="col-md-4">
+                        <h6>Terjual:</h6>
+                        <p>5.000</p>
+                      </div>
+                      <div class="col-md-4">
+                        <h6>Tersedia:</h6>
+                        <p>5.000</p>
+                      </div>
+                      <div class="col-md-4">
+                        <h6>Dikunjungi:</h6>
+                        <p>5.000</p>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style></style>
+<style>
+</style>
--- src/view/page/user/product.vue
+++ src/view/page/user/product.vue
@@ -1,9 +1,23 @@
 <template>
-    <section class="product">
+    <main class="tp-1">
         <div class="row">
-            <div class="col-lg-5 col-md-12 col-12">
-                <img class="img-fluid" src="https://via.placeholder.com/640x480.png/00bb99?text=animals+occaecati" alt="Eum magnam nihil sint.">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <img class="img-fluid" src="https://via.placeholder.com/640x480.png/00bb99?text=animals+occaecati"
+                    alt="Eum magnam nihil sint.">
+            </div>
+            <div class="col-lg-5 col-md-5 col-sm-12 ">
+                <h1 class="mb-1">Nama product</h1>
+                <div class="mt-2">
+                    <i class="fa fa-star text-warning"></i>
+                    <i class="fa fa-star text-warning"></i>
+                    <i class="fa fa-star text-warning"></i>
+                    <i class="fa fa-star text-warning"></i>
+                    <i class="fa-regular fa-star text-warning"></i>
+                </div>
+                <h4>
+                    <span>Ready</span>
+                </h4>
             </div>
         </div>
-    </section>
+    </main>
 </template>
