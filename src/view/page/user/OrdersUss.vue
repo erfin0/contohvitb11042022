9803f0299ea30f2084feffca941660139c3eb286
velarina <velarina2014@gmail.com>
2022-05-17 08:30:04 +0700

    order user

--- src/view/page/user/OrdersUss.vue
+++ src/view/page/user/OrdersUss.vue
@@ -23,7 +23,8 @@
             </div>
         </div>
     </nav>
-</div></div>
+</div>
+</div>
 
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 <div class="container">
