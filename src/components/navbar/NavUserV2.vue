d8ed983f6b0d2663187fb8cbc6edcdf396c92c8f
unabiinu <unabiinu@users.noreply.github.com>
2022-05-23 11:57:08 +0700

    navbar mobile responsive fix llo

--- src/components/navbar/NavUserV2.vue
+++ src/components/navbar/NavUserV2.vue
@@ -17,55 +17,49 @@ function logOut() {
 <template>
   <div class="sticky-bar">
     <div class="border-bottom">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid main-menu">
           <div class="logo">
             <router-link to="/shop" class="navbar-brand">Home</router-link>
-
           </div>
           <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-          <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto fs-6">
               <MenuuserVue />
             </ul>
-            <div class="collapse navbar-collapse justify-content-right">
-              <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                  <router-link to="/cart" class="btn btn-outline-dark">
-                    <i class="bi-cart-fill me-1"></i>
-                    Cart  
-                    <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
-                  </router-link>
-                </li>
-                <li class="nav-item">
-                  <template v-if="loggedIn">
-                    <a class="nav-link" @click.prevent="logOut"> LogOut </a>
-                  </template>
-                  <template v-else>
-                    <router-link to="/login" class="nav-link active">
-                      login
-                    </router-link>
-                  </template>
-                </li>
-              </ul>
+
+            <div class="d-flex">
+              <template v-if="loggedIn">
+                <router-link to="/cart" class="btn btn-outline-dark">
+                  <i class="bi-cart-fill me-1"></i>
+                  Cart
+                  <span class="badge bg-dark text-white ms-1 rounded-pill"
+                    >0</span
+                  >
+                </router-link>
+                <a class="nav-link" @click.prevent="logOut"> LogOut </a>
+              </template>
+              <template v-else>
+                <router-link to="/login" class="nav-link active">
+                  login
+                </router-link>
+              </template>
             </div>
           </div>
         </div>
       </nav>
     </div>
   </div>
-
 </template>
 
-<style>
-</style>
+<style></style>
