1da1ddc88bf268ae42b41c8361d5e21d4bd2db33
velarina <velarina2014@gmail.com>
2022-05-23 08:28:00 +0700

    checkout update

--- src/view/page/user/Checkout.vue
+++ src/view/page/user/Checkout.vue
@@ -1,94 +1,240 @@
 <template>
-<main class="tp-1">
-<div class="row">
-  <div class="col-75">
-    <div class="containerB">
-      <form action="/action_page.php">
-        <div class="rowB">
-          <div class="col-50">
-            <h3>Billing Address</h3>
-            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-            <input class="form-control"  type="text" id="fname" name="firstname" placeholder="John M. Doe">
-            <label for="email"><i class="fa fa-envelope"></i> Email</label>
-            <input class="form-control"  type="text" id="email" name="email" placeholder="john@example.com">
-            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-            <input class="form-control"  type="text" id="adr" name="address" placeholder="542 W. 15th Street">
-            <label for="city"><i class="fa fa-institution"></i> City</label>
-            <input class="form-control"  type="text" id="city" name="city" placeholder="New York">
-
-            <div class="rowB">
-              <div class="col-50">
-                <label for="state">State</label>
-                <input class="form-control"  type="text" id="state" name="state" placeholder="NY">
-              </div>
-              <div class="col-50">
-                <label for="zip">Zip</label>
-                <input class="form-control"  type="text" id="zip" name="zip" placeholder="10001">
-              </div>
-            </div>
-          </div>
-
-          <div class="col-50">
-            <h3>Payment</h3>
-            <label for="fname">Accepted Cards</label>
-            <div class="icon-container">
-              <i class="fa fa-cc-visa" style="color:navy;"></i>
-              <i class="fa fa-cc-amex" style="color:blue;"></i>
-              <i class="fa fa-cc-mastercard" style="color:red;"></i>
-              <i class="fa fa-cc-discover" style="color:orange;"></i>
-            </div>
-            <label for="cname">Name on Card</label>
-            <input class="form-control" type="text" id="cname" name="cardname" placeholder="John More Doe">
-            <label for="ccnum">Credit card number</label>
-            <input class="form-control"  type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
-            <label for="expmonth">Exp Month</label>
-            <input class="form-control" type="text" id="expmonth" name="expmonth" placeholder="September">
-
-            <div class="rowB">
-              <div class="col-50">
-                <label for="expyear">Exp Year</label>
-                <input class="form-control" type="text" id="expyear" name="expyear" placeholder="2018">
-              </div>
-              <div class="col-50">
-                <label for="cvv">CVV</label>
-                <input class="form-control" type="text" id="cvv" name="cvv" placeholder="352">
-              </div>
-            </div>
-          </div>
-
-        </div>
-        <label>
-          <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
-        </label>
-        <input type="submit" value="Continue to checkout" class="btnB">
-      </form>
-    </div>
-  </div>
-
-  <div class="col-25">
-    <div class="containerB">
-      <h4>Cart
-        <span class="price" style="color:black">
-          <i class="fa fa-shopping-cart"></i>
-          <b>4</b>
-        </span>
-      </h4>
-      <p><a href="#">Product 1</a> <span class="price">$15</span></p>
-      <p><a href="#">Product 2</a> <span class="price">$5</span></p>
-      <p><a href="#">Product 3</a> <span class="price">$8</span></p>
-      <p><a href="#">Product 4</a> <span class="price">$2</span></p>
-      <hr>
-      <p>Total <span class="price" style="color:black"><b>$30</b></span></p>
+<div class="row top">
+  <div class="col">
+    <div class="page-title-box">
+      <div class="page-title-right"></div>
+      <h4 class="page-title">Checkout</h4>
     </div>
   </div>
 </div>
-</main>
+<div class="row">
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="tab-content">
+              <div role="tabpanel" id="react-aria9399651908-6-tabpane-1" aria-labelledby="react-aria9399651908-6-tab-1" class="fade tab-pane active show">
+                <div class="row">
+                  <div class="col">
+                  <h4 class="mt-2">Billing information</h4>
+                  <p class="text-muted mb-4">Fill the form below in order to send you the order's invoice.</p>
+                  <form>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="mb-3">
+                        <label class="form-label">First Name</label>
+                          <input placeholder="Enter your first name" type="text" class="form-control">
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="mb-3">
+                        <label class="form-label">Last Name</label>
+                        <input placeholder="Enter your last name" type="text" class="form-control">
+                      </div>
+                    </div>
+                  </div> 
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="mb-3">
+                        <label class="form-label">Email address</label>
+                          <input placeholder="Enter your email" type="email" class="form-control">
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group mb-3">
+                        <label class="form-label">Phone</label>
+                          <input placeholder="(xxx) xxxx-xxxx" class="form-control" value="">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                  <div class="col-md-12">
+                    <div class="mb-3">
+                      <label class="form-label">Address</label>
+                        <input placeholder="Enter your state" type="text" class="form-control">
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-4">
+                    <div class="mb-3">
+                      <label class="form-label">Town / City</label>
+                        <input placeholder="Enter your city name" type="text" class="form-control">
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="mb-3">
+                      <label class="form-label">State</label>
+                        <input placeholder="Enter your state" type="text" class="form-control">
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="mb-3">
+                      <label class="form-label">Zip / Postal Code</label>
+                        <input placeholder="Enter your zip code" type="text" class="form-control">
+                    </div>
+                  </div>
+                </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="form-group mb-3">
+                    <label class="form-label">Country</label>
+                      <input placeholder="Enter your country" type="text" class="form-control">
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="mb-3">
+                    <label class="form-label">Order Notes:</label>
+                      <textarea placeholder="Write some note.." name="billing_notes" rows="3" type="textarea" class="form-control"></textarea>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-6">
+                  <a class="btn text-muted d-none d-sm-inline-block btn-link fw-semibold" href="/shop">
+                  <i class="mdi mdi-arrow-left"></i> Back to Shop </a>
+                </div>
+                <div class="text-sm-end col-sm-6"><button type="submit" class="btn btn-danger">
+                  <i class="mdi mdi-truck-fast me-1"></i> Proceed to Shipping</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <form class="mt-4 box">
+        <div class="row">
+          <div class="col">
+            <div class="mb-3">
+              <label class="form-label">Card Number</label>
+              <input placeholder="e.g. 4242 4242 4242 4242" name="card-number" type="text" class="form-control">
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="mb-3">
+              <label class="form-label">Name on Card</label>
+              <input placeholder="Enter your name" name="name" type="text" class="form-control">
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="mb-3">
+              <label class="form-label">Expiry date</label>
+              <input placeholder="MM/YY" class="form-control" value="">
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="mb-3">
+              <label class="form-label">CVV</label>
+              <input placeholder="xxx" class="form-control" value=""></div>
+            </div>
+          </div>
+      </form>
+    </div>
+    <div class="border p-3 mb-3 rounded">
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="form-check">
+            <input type="radio" id="BillingOptRadio3" name="billingOptions" class="form-check-input">
+              <label class="form-check-label font-16 fw-bold" for="BillingOptRadio3">Pay with Payoneer</label></div>
+              <p class="mb-0 ps-3 pt-1">You will be redirected to Payoneer website to complete your purchase securely.</p>
+          </div>
+          <div class="text-sm-end mt-3 mt-sm-0 col-sm-4">
+        </div>
+      </div>
+    </div>
+    <div class="border p-3 mb-3 rounded">
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="form-check">
+            <input type="radio" id="BillingOptRadio4" name="billingOptions" class="form-check-input">
+              <label class="form-check-label font-16 fw-bold" for="BillingOptRadio4">Cash on Delivery</label></div>
+                <p class="mb-0 ps-3 pt-1">Pay with cash when your order is delivered.</p>
+            </div>
+            <div class="text-sm-end mt-3 mt-sm-0 col-sm-4"> 
+          </div>
+        </div>
+      </div>
+      <div class="mt-4 row">
+        <div class="col-sm-6">
+          <a class="btn text-muted d-none d-sm-inline-block btn-link fw-semibold" href="/shop">
+            <i class="mdi mdi-arrow-left"></i> Back to Shop </a></div>
+              <div class="text-sm-end col-sm-6">
+                <button type="submit" class="btn btn-danger">
+                  <i class="mdi mdi-cash-multiple me-1"></i> Complete Order</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-4">
+    <div class="border p-3 mt-4 mt-lg-0 rounded">
+      <h4 class="header-title mb-3">Order Summary</h4>
+        <div class="table-responsive">
+          <table class="table table-centered mb-0">
+            <tbody>
+              <tr>
+                <td>
+                  <p class="m-0 d-inline-block align-middle">
+                    <a class="text-body fw-semibold">Amazing Modern Chair</a>
+                    <br>
+                    <small>5 x $148.66</small>
+                  </p>
+                </td>
+                <td class="text-end">$743.30</td>
+              </tr>
+              <tr>
+                <td>
+                  <p class="m-0 d-inline-block align-middle">
+                    <a class="text-body fw-semibold">Biblio Plastic Armchair</a>
+                    <br>
+                    <small>2 x $99.00</small>
+                  </p>
+                </td>
+                <td class="text-end">$198.00</td>
+              </tr>
+              <tr>
+                <td>
+                  <p class="m-0 d-inline-block align-middle">
+                    <a class="text-body fw-semibold">Designer Awesome Chair</a>
+                    <br>
+                    <small>3 x $49.99</small></p>
+                </td>
+                <td class="text-end">$499.90</td>
+              </tr>
+              <tr class="text-end">
+                <td>
+                  <h6 class="m-0">Sub Total:</h6>
+                </td>
+                <td class="text-end">$1071.29</td>
+              </tr>
+              <tr class="text-end">
+                <td>
+                  <h6 class="m-0">Shipping:</h6>
+                </td>
+                <td class="text-end">FREE</td>
+              </tr>
+              <tr class="text-end">
+                <td>
+                  <h5 class="m-0">Total:</h5>
+                </td>
+                <td class="text-end fw-semibold">$1071.29</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+ </div>
 
+                              
 </template>
-
-<script setup>
-
-</script>
 
 <style>
 @import '../../../assets/css/checkoutUss.css';
